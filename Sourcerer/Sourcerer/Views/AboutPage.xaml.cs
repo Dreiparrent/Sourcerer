c214e4848a917edb916a299b6ad59ae9fe6e071c
DreiParrent <dreiparrent@gmail.com>
2018-07-22 10:01:57 -0600

    Add project files.

---
+++ Sourcerer/Sourcerer/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sourcerer.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
